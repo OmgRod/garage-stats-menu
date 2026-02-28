9dde5a4016d4d7d8ee94c12f4dc4dad39e366b16
NyteLyte <nytelyte@tuta.io>
2026-02-28 03:18:49 +0100

    fix menu positioning, deduplicate code

--- src/layers/StatsGarageLayer.cpp
+++ src/layers/StatsGarageLayer.cpp
@@ -5,9 +5,35 @@
 
 using namespace geode::prelude;
 
-// for some reason, if i cast to PageMenu, i can't actually get m_fields->m_elementCount; it's just 0.
-// so we're making it a constant i guess
-const int ELEMENTS_PER_PAGE = 15;
+const int ELEMENTS_PER_PAGE = 10;
+// the distance of the first label from the top of the screen
+const float TOP_MARGIN = 6.f;
+// the distance from the right side of the screen
+float RIGHT_MARGIN = 18.f;
+// the pages api arrow scale
+float ARROW_SCALE = 0.6f;
+// the pages api arrow padding
+float ARROW_PADDING = 1.f;
+
+inline void addDefaultItem(
+	CCNode* statsMenu,
+	const char* setting,
+	const char* id,
+	const char* spriteFrameName,
+	const char* statNum,
+	float scale
+) {
+	if (Mod::get()->getSettingValue<bool>(setting)) {
+		statsMenu->addChild(
+			StatsDisplayAPI::getNewItem(
+				id,
+				CCSprite::createWithSpriteFrameName(spriteFrameName),
+				GameStatsManager::sharedState()->getStat(statNum),
+				scale
+			)
+		);
+	}
+}
 
 bool StatsGarageLayer::init() {
 	if (!GJGarageLayer::init())
@@ -18,78 +44,108 @@ bool StatsGarageLayer::init() {
 
 	m_fields->m_statsMenu = CCMenu::create();
 
-	/*if (mod->getSettingValue<bool>("stars-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("stars"));
-	if (mod->getSettingValue<bool>("moons-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("moons"));
-	if (mod->getSettingValue<bool>("gold-coins-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("coins"));
-	if (mod->getSettingValue<bool>("user-coins-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("user-coins"));
-	if (mod->getSettingValue<bool>("orbs-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("orbs"));
-	if (mod->getSettingValue<bool>("diamonds-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("diamonds"));
-	if (mod->getSettingValue<bool>("diamond-shards-stat")) m_fields->m_nodeContainer.push_back(getExistingContainer("diamond-shards"));
-
-	for (size_t i = 0; i < m_fields->m_nodeContainer.size(); i++) {
-		//m_fields->m_nodeContainer[i]->setLayout(AxisLayout::create()->setAutoScale(false)->setAxisReverse(true));
-		m_fields->m_statsMenu->addChild(m_fields->m_nodeContainer[i]);
-	}*/
-
 	m_fields->m_statsMenu->setID("stats-menu"_spr);
 	m_fields->m_statsMenu->setZOrder(2);
-	m_fields->m_statsMenu->setLayout(ColumnLayout::create()->setAutoScale(false)->setAxisReverse(true)->setAxisAlignment(AxisAlignment::End)->setCrossAxisAlignment(AxisAlignment::End)->setGap(15)->setAutoScale(true));
+	m_fields->m_statsMenu->setLayout(
+		ColumnLayout::create()
+		->setAxisReverse(true)
+		->setCrossAxisAlignment(AxisAlignment::End)
+		->setGap(15)
+	);
 	
 	this->addChild(m_fields->m_statsMenu);
-	if (mod->getSettingValue<bool>("stars-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("stars", CCSprite::createWithSpriteFrameName("GJ_starsIcon_001.png"), GameStatsManager::sharedState()->getStat("6"), 0.54f));
-	if (mod->getSettingValue<bool>("moons-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("moons", CCSprite::createWithSpriteFrameName("GJ_moonsIcon_001.png"), GameStatsManager::sharedState()->getStat("28"), 0.54f));
-	if (mod->getSettingValue<bool>("gold-coins-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("coins", CCSprite::createWithSpriteFrameName("GJ_coinsIcon_001.png"), GameStatsManager::sharedState()->getStat("8"), 0.51f));
-	if (mod->getSettingValue<bool>("user-coins-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("user-coins", CCSprite::createWithSpriteFrameName("GJ_coinsIcon2_001.png"), GameStatsManager::sharedState()->getStat("12"), 0.51f));
-	if (mod->getSettingValue<bool>("orbs-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("orbs", CCSprite::createWithSpriteFrameName("currencyOrbIcon_001.png"), GameStatsManager::sharedState()->getStat("14"), 0.54f));
-	if (mod->getSettingValue<bool>("diamonds-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("diamonds", CCSprite::createWithSpriteFrameName("GJ_diamondsIcon_001.png"), GameStatsManager::sharedState()->getStat("13"), 0.6f));
-	if (mod->getSettingValue<bool>("diamond-shards-stat")) m_fields->m_statsMenu->addChild(StatsDisplayAPI::getNewItem("diamond-shards", CCSprite::createWithSpriteFrameName("currencyDiamondIcon_001.png"), GameStatsManager::sharedState()->getStat("29"), 0.54f));
+	addDefaultItem(m_fields->m_statsMenu, "stars-stat", "stars", "GJ_starsIcon_001.png", "6", 0.54f);
+	addDefaultItem(m_fields->m_statsMenu, "moons-stat", "moons", "GJ_moonsIcon_001.png", "28", 0.54f);
+	addDefaultItem(m_fields->m_statsMenu, "gold-coins-stat", "coins", "GJ_coinsIcon_001.png", "8", 0.51f);
+	addDefaultItem(m_fields->m_statsMenu, "user-coins-stat", "user-coins", "GJ_coinsIcon2_001.png", "12", 0.51f);
+	addDefaultItem(m_fields->m_statsMenu, "orbs-stat", "orbs", "currencyOrbIcon_001.png", "14", 0.54f);
+	addDefaultItem(m_fields->m_statsMenu, "diamonds-stat", "diamonds", "GJ_diamondsIcon_001.png", "13", 0.6f);
+	addDefaultItem(m_fields->m_statsMenu, "diamond-shards-stat", "diamond-shards", "currencyDiamondIcon_001.png", "29", 0.54f);
 
-	m_fields->m_statsMenu->setPosition(ccp(winSize.width - 18, winSize.height - 12));
-
-	auto tempSprite = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png");
-	if (tempSprite) {
-		float height = tempSprite->getScaledContentHeight();
-		tempSprite->release();
-		m_fields->m_statsMenu->setContentHeight(winSize.height - 24 - height);
-	}
-
-	m_fields->m_statsMenu->setAnchorPoint(ccp(0.5f, 1.f));
-
-	m_fields->m_statsMenu->updateLayout();
+	
+	float bottomMargin = CCSprite::createWithSpriteFrameName("GJ_sideArt_001.png")->getContentHeight();
+	float arrowSize = ARROW_SCALE * CCSprite::createWithSpriteFrameName("GJ_arrow_02_001.png")->getContentWidth();
+	
+	// the maximum height that our elements should cover, including the arrows and their paddings as "elements"
+	float maxHeight = winSize.height - TOP_MARGIN - bottomMargin;
+	// the actual maximum of the content, excluding the arrows
+	m_fields->m_statsMenu->setContentSize({0, maxHeight - 2*ARROW_PADDING - 2*arrowSize});
+	m_fields->m_statsMenu->setAnchorPoint(CCPoint{0.5f, 1.f});
+	m_fields->m_statsMenu->setPositionX(winSize.width - RIGHT_MARGIN);
 
 	if (PagesAPI::isLoaded()) {
-    schedule(schedule_selector(StatsGarageLayer::moveMenuForArrows));
     PageMenu* pageMenu = static_cast<PageMenu*>(m_fields->m_statsMenu); 
-		pageMenu->setPaged(ELEMENTS_PER_PAGE, PageOrientation::VERTICAL, m_fields->m_statsMenu->getContentHeight(), 12.f);
-		pageMenu->setButtonScale(0.6f);
+		pageMenu->setPaged(ELEMENTS_PER_PAGE, PageOrientation::VERTICAL, maxHeight, ARROW_PADDING);
+		pageMenu->setButtonScale(ARROW_SCALE);
+		schedule(schedule_selector(StatsGarageLayer::moveMenuForArrows));
 	}
 
 	return true;
 }
 
-/*CCNode* StatsGarageLayer::getExistingContainer(std::string itemName) {
-	auto ret = CCMenu::create(); 
-	auto icon = this->getChildByID(itemName + "-icon");
-	if (icon) {
-		icon->removeFromParentAndCleanup(false);
-		ret->addChild(icon);
-		icon->setPosition({0, 0});
-	}
-	auto label = this->getChildByID(itemName + "-label"); 
-	if (label) {	
-		label->removeFromParentAndCleanup(false);
-		ret->addChild(label);
-		label->setPosition({-12, 0.5});
-	}
-	ret->setID(""_spr + itemName + "-container");
-	ret->setContentSize({ 0, 0 });
-	return ret;
-}*/
+CCNode* getFirstVisibleChild(CCNode* parent) {
+	for (auto child : parent->getChildrenExt())
+		if (child->isVisible())
+			return child;
+	return nullptr;
+}
 
+CCNode* getLastVisibleChild(CCNode* parent) {
+	CCArrayExt<CCNode*> arrayExt = parent->getChildrenExt();
+	if (arrayExt.size() == 0)
+		return nullptr;
+	for (size_t i = arrayExt.size()-1; i >= 0; i--)
+		if (arrayExt[i]->isVisible())
+			return arrayExt[i];
+	return nullptr;
+}
+
+// this is a hack to move the node properly so it works with the way pages api works
+// another issue this has it that if you switch a page and it needs to move the menu
+// the arrows will visually jitter for a frame
+// i do not know how to fix this without modifying pages api to add button press callbacks
+// so we can do this instantly on button press
+// it would genuinely be much easier to implement your own paging at this point
 void StatsGarageLayer::moveMenuForArrows(float) {
   auto winSize = CCDirector::get()->getWinSize();
-  if (m_fields->m_statsMenu->getChildrenCount() > ELEMENTS_PER_PAGE)
-    m_fields->m_statsMenu->setPosition(ccp(winSize.width - 18, winSize.height - 30));
-  else
-    m_fields->m_statsMenu->setPosition(ccp(winSize.width - 18, winSize.height - 12));
+	CCNode* firstChild = getFirstVisibleChild(m_fields->m_statsMenu);
+	CCNode* lastChild = getLastVisibleChild(m_fields->m_statsMenu);
+	if (!firstChild || !lastChild) return;
+	CCNode* firstLabel = firstChild->getChildByIndex(1);
+	CCNode* lastLabel = lastChild->getChildByIndex(1);
+	if (!firstLabel || !lastLabel) return;
+
+	float topLabelMaxY = firstChild->convertToWorldSpace(CCPoint{0, firstLabel->boundingBox().getMaxY()}).y;
+	float bottomLabelMinY = lastChild->convertToWorldSpace(CCPoint{0, lastLabel->boundingBox().getMinY()}).y;
+	
+	float newTopLabelMaxY;
+	if (CCNode* pageMenu = m_fields->m_statsMenu->getParent()->getChildByID("stats-menu-navigation-menu"_spr)) {
+		CCNode* previous = pageMenu->getChildByID("prev-button");
+		CCNode* next = pageMenu->getChildByID("next-button");
+		if (!previous || !next) return;
+
+		float previousMaxY = pageMenu->convertToWorldSpace(CCPoint{0, previous->boundingBox().getMaxY()}).y;
+		float nextMinY = pageMenu->convertToWorldSpace(CCPoint{0, next->boundingBox().getMinY()}).y;
+		
+		float prevDelta = previousMaxY - topLabelMaxY - ARROW_PADDING - previous->getScaledContentHeight();
+		float nextDelta = bottomLabelMinY - nextMinY - ARROW_PADDING - next->getScaledContentHeight();
+		previous->setPositionY(previous->getPositionY() - prevDelta);
+		next->setPositionY(next->getPositionY() + nextDelta);
+		// if the current top label's max y is topLabelMaxY
+		// we want to move everything so that
+		// winSize.height - newTopLabelMaxY = previous->getContentHeight() + ARROW_PADDING + TOP_MARGIN
+		// newTopLabelMaxY = winSize.height - previous->getContentHeight() - ARROW_PADDING - TOP_MARGIN
+		// we don't multiply the height by button scale because the scale is on the inner sprite,
+		// not the button itself, so its content height is adjusted to the scale already
+		// if we don't have arrows, we exclude them and their padding from the equation and thus get the 2nd branch
+		// newTopLabelMaxY = winSize.height - TOP_MARGIN
+		newTopLabelMaxY = winSize.height - previous->getContentHeight() - ARROW_PADDING - TOP_MARGIN;
+	} else {
+		// if the arrow menu does not exist, then we only have a single page and do not need to
+		// keep track of anything else, just move the menu to align with the top margin
+		newTopLabelMaxY = winSize.height - TOP_MARGIN;
+	}
+	// the amount we need to move the menu so that the top label max y becomes newTopLabelMaxY
+	float topLabelMaxYDelta = topLabelMaxY - newTopLabelMaxY;
+	m_fields->m_statsMenu->setPositionY(m_fields->m_statsMenu->getPositionY() - topLabelMaxYDelta);
 }
